0e07c65278733ffd272ad291adc8cc757c7b285f
root <lcarcases@gmail.com>
2021-12-28 17:42:45 -0600

    cicd

--- src/tests/src/components/Header.test.js
+++ src/tests/src/components/Header.test.js
@@ -39,7 +39,6 @@ describe('Pruebas Header', () => {
     test('debe de mostrar el subtitulo enviado por props', () => {
         
         const p = "1234";
-
         const test = true;
         const username = 'lcarcases4';
         const stockName = "STOCKS ANALYSIS APP  ";
