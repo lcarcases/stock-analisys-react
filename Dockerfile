214caade8c3a87e4085176187933f8b1c1fbb30f
root <lcarcases@gmail.com>
2021-12-28 17:17:46 -0600

    cicd

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,10 @@ RUN npm install -g npm@7.24.0
 
 RUN npm install
 
+RUN npm install --save-dev @wojtekmaj/enzyme-adapter-react-17
+
+RUN npm install --save-dev enzyme-to-json
+
 EXPOSE 3001
 
 CMD ["npm", "start", "-y"]
---
+++ src/tests/src/components/Header.test.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import '@testing-library/jest-dom'
+import { shallow } from 'enzyme'
+
+import Header from '../../../components/header/Header';
+
+
+describe('Pruebas Header', () => {
+    
+    test('debe de mostrar <Header /> correctamente', () => {
+
+        const test = true;
+        const username = 'lcarcases4';
+        const stockName = "STOCKS ANALYSIS APP  ";
+
+        let headerStyles = {};
+        headerStyles.stockName = {};
+        headerStyles.searchInput = {};
+
+        headerStyles.stockName.marginLeft = '5rem';
+        headerStyles.searchInput.marginLeft = '-1.8rem';
+        if(window.innerWidth > 375 && window.innerWidth <= 900) {
+            headerStyles.searchInput.marginLeft = '-7.8rem';
+        }
+
+        const wrapper = shallow(<Header className="header"
+                                        stockName={stockName}
+                                        userName={username}
+                                        headerStyles={headerStyles}>
+                                </Header>
+                               );
+
+        expect( wrapper ).toMatchSnapshot();
+        
+    });
+
+    test('debe de mostrar el subtitulo enviado por props', () => {
+        
+        const test = true;
+        const username = 'lcarcases4';
+        const stockName = "STOCKS ANALYSIS APP  ";
+
+        let headerStyles = {};
+        headerStyles.stockName = {};
+        headerStyles.searchInput = {};
+
+        headerStyles.stockName.marginLeft = '5rem';
+        headerStyles.searchInput.marginLeft = '-1.8rem';
+        if(window.innerWidth > 375 && window.innerWidth <= 900) {
+            headerStyles.searchInput.marginLeft = '-7.8rem';
+        }
+
+        const wrapper = shallow(<Header className="header"
+                                        stockName={stockName}
+                                        userName={username}
+                                        headerStyles={headerStyles}>
+                                </Header>
+                               );
+
+        const spanText = wrapper.find('span').first().text();
+        
+        expect( spanText ).toBe( stockName );
+
+    })
+    
+    
+    
+
+})
